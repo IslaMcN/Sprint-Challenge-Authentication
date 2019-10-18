b88bfee51f87b3dd18de515a4d495102e956679a
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-18 08:58:54 -0700

    ugh

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -8,7 +8,7 @@ const secrets = require('../config/secrets');
 router.post('/register', (req, res) => {
   // implement registration
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 27);
+  const hash = bcrypt.hashSync(user.password, 8);
   user.password = hash;
 
   Users.add(user)
@@ -19,6 +19,7 @@ router.post('/register', (req, res) => {
     res.status(500).json({
       message: 'Sorry you cannot be added because you arent cool enough.', err
     });
+    console.log(err)
   });
 });
 
@@ -29,9 +30,11 @@ router.post('/login', (req, res) => {
   Users.findBy({username})
   .first()
   .then(user => {
+    console.log(hi)
     if (user && bcrypt.compareSync(password, user.password)){
+      console.log(hello)
       const token = generateToken(user);
-
+      console.log(hi)
       res.status(200).json({
         message: `Welcome ${user.username}!`
       });
@@ -52,6 +55,9 @@ function generateToken(user){
     username: user.username,
     subject: user.id,
   };
+  const options = {
+    expiresIn: '27d'
+  }
 
   return jwt.sign(payload, secrets.jwtSecret, options);
 }
--- users/users-routermodel.js
+++ users/users-routermodel.js
@@ -8,21 +8,21 @@ module.exports = {
 };
 
 function find(){
-    return db('user').select('id', 'username', 'department');
+    return db('users').select('id', 'username');
 }
 
 function findBy(filter){
-    return db('user').where(filter);
+    return db('users').where(filter);
 }
 
 async function add(user){
-    const [id] = await db('user').insert(user);
+    const [id] = await db('users').insert(user);
     return findById(id);
 }
 
 function findById(id){
-    return db('user')
-    .select('id', 'username', 'department')
+    return db('users')
+    .select('id', 'username')
     .where({id})
     .first();
 }
