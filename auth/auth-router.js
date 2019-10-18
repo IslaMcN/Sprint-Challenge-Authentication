ab08277b421284c003077a2263281e785df6c4a5
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-18 08:29:46 -0700

    middleware done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,14 +3,58 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-routermodel');
-const secrets = require('../')
+const secrets = require('../config/secrets');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 27);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved);
+  })
+  .catch(err => {
+    res.status(500).json({
+      message: 'Sorry you cannot be added because you arent cool enough.', err
+    });
+  });
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let {username, password} = req.body;
+
+  Users.findBy({username})
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)){
+      const token = generateToken(user);
+
+      res.status(200).json({
+        message: `Welcome ${user.username}!`
+      });
+      console.log(token)
+    }else{
+      res.status(401).json({
+        message: "I dont even know you"
+      });
+    }
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
+
+function generateToken(user){
+  const payload = {
+    username: user.username,
+    subject: user.id,
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
 
 module.exports = router;
